42c73b5e754cf9ffc118264a3ced8dbbee85e86c
Leland Batey <lelandbatey@gmail.com>
2015-09-30 16:26:22 -0700

    Clean `flask_slides` to conform to pylint

--- flask_slides.py
+++ flask_slides.py
@@ -1,15 +1,19 @@
+# -*- coding: utf-8 -*-
+"""An API for interacting with the slides stored in the `slides/` directory."""
+
 from __future__ import print_function
-import markdown
-from os.path import join, abspath, isfile, basename, getsize
-from time import sleep
+from os.path import join, abspath
 import directory_cache
+import markdown
 import os.path
 import json
 import os
+# pylint: disable=W0312
+
 
 ################  Flask_Slides   ################
-# 
-# 
+#
+#
 ###             What:            ###
 #
 # Flask_slides takes the simple model of slides as files in a directory and
@@ -25,18 +29,20 @@ import os
 #
 
 
-def jsonDump(inDat):
-	return json.dumps(inDat, sort_keys=True, indent=4, separators=(',', ': '))
-def jp(inDat):
-	print(jsonDump(inDat))
+def jdump(obj):
+	"""Return json string representation of object"""
+	return json.dumps(obj, sort_keys=True, indent=4, separators=(',', ': '))
+def jprint(obj):
+	"""Print a json representation of a given object"""
+	print(jdump(obj))
 
 def strip_ext(files):
-	"""Given a file name or list of file names, strips the extension from each file name."""
+	"""Strips the extension from a filename or list of filenames"""
 	if   isinstance(files, basestring):
 		return '.'.join(files.split('.')[:-1])
 	elif isinstance(files, list):
 		# List comprehension of the above stripping process.
-		return [ '.'.join(f.split('.')[:-1]) for f in files ]
+		return ['.'.join(f.split('.')[:-1]) for f in files]
 
 
 
@@ -46,42 +52,43 @@ class flask_slides(object):
 		# super(flask_slides, self).__init__()
 		self.slides_dir = slides_dir
 		self._index = 0
-		self.noneStr = "<h1>No data for that slide</h1>"
+		self.invalid_slide = "<h1>No data for that slide</h1>"
 		self.cache = directory_cache.directory_cache(self.slides_dir)
-		
+
 
 	def get_slide_list(self):
 		"""Returns sorted list of slides in the slide directory"""
 		return self.cache.get_files_list()
-		
+
 	def get_slides(self):
 		"""Returns dictionary of slide information for slides in slides_dir.
 
-		Keys in the dictionary are the names of the slide files, with only the file extension removed.
-		Values in the dict are the full paths to the slide files in the slides_dir.
+		Keys in the dictionary are the names of the slide files, with only the
+		file extension removed.  Values in the dict are the full paths to the
+		slide files in the slides_dir.
 		"""
 		slides = self.get_slide_list()
-		toRet = {}
+		ret_val = {}
 
-		for x in slides:
-			toRet[strip_ext(x)] = abspath(join(self.slides_dir,x))
+		for slide in slides:
+			ret_val[strip_ext(slide)] = abspath(join(self.slides_dir, slide))
 
-		return toRet
+		return ret_val
 
 	def render_slide(self, slide_name):
 		"""Return string of rendered contents of the given slide.
 		Valid slide names are 'slide_name' or 'slide_name.ext'
 
-		If the slide exists, gets everything between the strings "<slide>" and "</slide>".
-		If it doesn't exist, returns string "Does not exist".
+		If the slide exists, gets everything between the strings "<slide>" and
+		"</slide>".  If it doesn't exist, returns string "Does not exist".
 		"""
-		
+
 		slides = self.get_slides()
 		slide_path = ""
 		slide = ""
 
 		# Try to match the given slide to the path of the actual file
-		for x in slides:
+		for _ in slides:
 			# If name format is "slide_name.extension"
 			if slide_name in self.cache.keys():
 				slide_path = self.cache[slide_name]
@@ -92,7 +99,8 @@ class flask_slides(object):
 					if self.cache[k]['no_ext'] == slide_name:
 						slide_path = k
 						break
-				if slide_path: break
+				if slide_path:
+					break
 
 		if slide_path:
 			# Get contents of slide
@@ -106,34 +114,39 @@ class flask_slides(object):
 
 		# If no slide was found, or if the slide was empty
 		if not slide:
-			slide = self.noneStr
+			slide = self.invalid_slide
 
+		extension = ['codehilite(css_class=highlight,linenums=True)']
 		# Pass the slide through a markdown renderer. This preserves html, if
 		# it's entered, but parses markdown into html. Allows for mixing of
 		# the two.
-		slide = markdown.markdown(slide, safe_mode=False, extensions=['codehilite(css_class=highlight,linenums=True)'])
+		slide = markdown.markdown(slide, safe_mode=False, extensions=extension)
 
 		return slide
 
 	def render_index(self, slide_number):
 		"""Given an index, render that slide (in the series of slides)."""
 		self.index = slide_number
-		i = 0
 		slide_list = sorted(self.get_slides())
-		for x in slide_list:
-			if i == slide_number:
-				return self.render_slide(x)
-			i += 1
+		for idx, value in slide_list:
+			if idx == slide_number:
+				return self.render_slide(value)
 
-		return self.noneStr
+		return self.invalid_slide
 
 	# Creating properties for index so we can keep it from dropping below 0 or
 	# going beyond the number of total slides.
 	@property
 	def index(self):
+		"""Return value of underlying index value"""
 		return self._index
 	@index.setter
 	def index(self, value):
+		"""Set index as property.
+		
+		Done so index is always greater than 0 or less than the total number of
+		slides.
+		"""
 		slide_count = len(self.get_slide_list())-1
 		if value < 0:
 			self._index = 0
@@ -144,16 +157,5 @@ class flask_slides(object):
 
 
 if __name__ == '__main__':
-	f = flask_slides()
-	jp(f.get_slides())
-
-
-
-
-
-
-
-
-
-
+	jprint(flask_slides().get_slides())
 
