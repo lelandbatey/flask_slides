b3db88af8750271cc377c5d96da47437de5044f1
Leland Batey <lelandbatey@gmail.com>
2015-10-01 11:40:29 -0700

    Clean `directory_cache.py` to follow pylint rules

--- directory_cache.py
+++ directory_cache.py
@@ -1,20 +1,27 @@
+# -*- coding: utf-8 -*-
+
+"""
+In-memory copy of files in a given directory, updated when files are added or
+updated.
+"""
+
 from __future__ import print_function
-from os.path import join, abspath, isfile, basename, getsize
+from os.path import join, abspath, isfile, getsize
 from threading import Thread
 from time import sleep
 import os.path
-import time
 import json
 import os
+# pylint: disable=W0312
 
 ################ Directory Cache ################
-# 
-# 
+#
+#
 ### What is the purpose of this: ###
 #
 # I wrote this as a simple in-memory copy of the files found on disk. All the
 # files are read into memory at the beginning, and they're re-read into memory
-# if they're modified, or if new files are added. 
+# if they're modified, or if new files are added.
 #
 # There's a simple thread that is perpetually checking the directory and
 # updating a data structure with the names and sizes of all files in the
@@ -50,61 +57,74 @@ import os
 #
 
 
-def jsonDump(inDat):
-	return json.dumps(inDat, sort_keys=True, indent=4, separators=(',', ': '))
-def jp(inDat):
-	print(jsonDump(inDat))
+def jdump(obj):
+	"""Return json string representation of object"""
+	return json.dumps(obj, sort_keys=True, indent=4, separators=(',', ': '))
 
 # Gets contents of a file
 def get_file_contents(file_name):
-	with open(file_name, 'r') as f: toRet = f.read().decode('utf-8')
-	return toRet
+	"""Read a text file conveniently and properly."""
+	ret_val = None
+	with open(file_name, 'r') as tmp_file:
+		ret_val = tmp_file.read().decode('utf-8')
+	return ret_val
 
 def get_dir_files(path):
+	"""Return list of files in directory."""
 	data_dir = os.path.abspath(path)
-	files = [ f for f in os.listdir(data_dir) if isfile(join(data_dir, f))]
+	files = [f for f in os.listdir(data_dir) if isfile(join(data_dir, f))]
 	return sorted(files)
 
 def build_file_size_dir(path):
-	return {f : getsize(join(path,f)) for f in get_dir_files(path)}
+	"""Return dictionary of all files in a directory with their size."""
+	return {f : getsize(join(path, f)) for f in get_dir_files(path)}
 
 
 def strip_ext(files):
-	"""Given a file name or list of file names, strips the extension from each file name."""
+	"""Strips the extension from a filename or list of filenames"""
 	if   isinstance(files, basestring):
 		return '.'.join(files.split('.')[:-1])
 	elif isinstance(files, list):
 		# List comprehension of the above stripping process.
-		return [ '.'.join(f.split('.')[:-1]) for f in files ]
+		return ['.'.join(f.split('.')[:-1]) for f in files]
 
 
 class directory_cache(object):
 	"""Cache of all files in root level of a directory.
 
-	Only works with utf-8 encoded text files, since that's what I'm going to be dealing with.
+	Only works with utf-8 encoded text files, since that's what I'm going to be
+	dealing with.
+
+	Initially loads the contents of a directory into memory, then spawns a
+	thread that checks every half second if the contents of the underlying
+	directory have been modified. If the on-disk contents have been modified,
+	reloads them into memory. The check for updated files is done lazily, so
+	only when attempting to access the cache.
 	"""
 	def __init__(self, data_dir):
-		# super(directory_cache, self).__init__()
 		self.data_dir = data_dir
 		self._cache = {}
 		self.sizes_dict = {}
 
-
-		# Used to watch the directory for changes in a non-blocking, multi-threaded way
-		self.procThread = Thread(target=self.build_file_size_dict)
-		self.procThread.daemon = True # When the program dies, our thread dies as well.
-		self.procThread.start()
+		# Used to watch the directory for changes in a non-blocking,
+		# multi-threaded way.
+		self.process_thread = Thread(target=self.build_file_size_dict)
+		# Ensures that when the program dies, our thread dies as well.
+		self.process_thread.daemon = True
+		self.process_thread.start()
 
 		self.build_cache()
 
 	def get_files_list(self):
 		"""Returns sorted list of files in the data_dir directory"""
 		data_dir = os.path.abspath(self.data_dir)
-		files = [ f for f in os.listdir(data_dir) if isfile(join(data_dir, f))]
+		files = [f for f in os.listdir(data_dir) if isfile(join(data_dir, f))]
 		return sorted(files)
-		
+
 	def build_file_size_dict(self):
-		# Every 0.5 seconds, re-evaluates the size of all the files in the watched directory
+		"""Rebuild the size of the directory."""
+		# Every 0.5 seconds, re-evaluates the size of all the files in the
+		# watched directory
 		while True:
 			self.sizes_dict = build_file_size_dir(self.data_dir)
 			sleep(0.5)
@@ -115,17 +135,16 @@ class directory_cache(object):
 		The cache is considered outdated if any files have been added to the
 		watched directory, or if any of the files within that directory differ
 		in size from the cached version.
-
 		"""
 		current_state = self.sizes_dict
 		current_cache = self._cache
 
-		current_cache = { f : current_cache[f]['size'] for f in current_cache }
+		current_cache = {f : current_cache[f]['size'] for f in current_cache}
 
-		for x in current_state:
-			if x not in current_cache.keys():
+		for file_name in current_state:
+			if file_name not in current_cache.keys():
 				return True
-			elif current_state[x] != current_cache[x]:
+			elif current_state[file_name] != current_cache[file_name]:
 				return True
 
 		return False
@@ -133,31 +152,33 @@ class directory_cache(object):
 	def build_cache(self):
 		"""Builds (or re-builds) the `self.cache` object"""
 		t_cache = {}
-		for x in self.get_files_list():
-			path = abspath(join(self.data_dir,x))
-			t_cache[x] = {
-				'path'   : path,
-				'data'   : get_file_contents(path),
-				'no_ext' : strip_ext(x),
-				'size'   : getsize(path)
-			}
+		for file_name in self.get_files_list():
+			path = abspath(join(self.data_dir, file_name))
+			t_cache[file_name] = {'path'   : path,
+			                      'data'   : get_file_contents(path),
+			                      'no_ext' : strip_ext(file_name),
+			                      'size'   : getsize(path)}
 		self.cache = t_cache
 
 	@property
 	def cache(self):
+		"""Returns existing cache or updates cache if outdated"""
 		if self.is_cache_outdated():
 			self.build_cache()
 		return self._cache
 	@cache.setter
 	def cache(self, value):
+		"""Return underlying cache object"""
 		self._cache = value
 
 	def __repr__(self):
-		return jsonDump(self.cache)
+		"""Return string version of directory cache"""
+		return jdump(self.cache)
 
 	def __getitem__(self, key):
-		# Allows for dictionary-like access directly to the self.cache item
+		"""Implement dictionary-like access directly to the self.cache object"""
 		return self.cache[key]
 
 	def keys(self):
+		"""Return the keys for underlying _cache object"""
 		return self.cache.keys()
